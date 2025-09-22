700177acfc80d9717b16fb7835182d1463f26486
codeby-han <hangyeolg84@gmail.com>
2025-09-22 16:19:48 +0900

    asdas

--- svelte.config.js
+++ svelte.config.js
@@ -4,11 +4,7 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
     kit: {
         adapter: adapter({
-            pages: 'build',
-            assets: 'build',
             fallback: 'index.html',
-            precompress: false,
-            strict: true,
         }),
         paths: {
             base: process.env.NODE_ENV === 'production' ? '/sharkSSS-dev.github.io' : '',
